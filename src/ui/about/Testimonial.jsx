914c679e80e42e23ae664e46dccab448dbc1379a
Olakareem <olakareemomobolarinwa@gmail.com>
2025-12-22 12:18:01 +0100

    Update Next.js to fix security vulnerability CVE-2025-66478

--- src/ui/about/Testimonial.jsx
+++ src/ui/about/Testimonial.jsx
@@ -4,10 +4,11 @@ import H2 from "@/components/Typography";
 
 const Testimonial = () => {
   return (
-    <div>
+    <div       className="max-w-[1440px] px-18 mx-auto py-20 flex flex-col gap-12"
+>
           <H2> PBR FINAL FINAL INTERVIEW WITH DEXA TEAM</H2>
 
-         <div className="w-full rounded-xl overflow-hidden p-32">
+         <div className="w-full rounded-xl overflow-hidden ">
                 <VideoPlayer
                   src="/videos/1222.mp4"
                   className="w-full h-full object-cover rounded-xl"
